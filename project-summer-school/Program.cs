ecc3b881074754834a1a801e9dd8d830802eb9d2
jkirouac42 <jkirouac42@gmail.com>
2016-06-19 14:46:44 -0400

    if else method

--- project-summer-school/Program.cs
+++ project-summer-school/Program.cs
@@ -12,16 +12,15 @@ namespace project_summer_school
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to the Hogwart's Student Enrollment System");
-            Console.WriteLine("Please select from our menu.\nEnrollment select 1");
-            Console.WriteLine("To remove student select 2");
+            Console.WriteLine("To enroll a student please select 1.");
+            
             while (true)
             {
                 int choice = Convert.ToInt32(Console.ReadLine());
                 if (choice == 1)
                 {
                     EnrollStudent();//static void method
-                    Console.WriteLine("Should you want to enroll another\nPlease press 1");
-                    Console.WriteLine("To return to the main menu please press 5");
+                    Console.WriteLine("Enter name to enroll another press 1");
                 }
                 else if (choice == 2)
                 {
@@ -35,36 +34,96 @@ namespace project_summer_school
             Console.WriteLine("Enter students name.");
             string[] newStudent = new string[15];
             string student = Console.ReadLine();
-            
-            for (int index = 0; index < newStudent.Length; index ++)
+
+            if (newStudent[0] == null)
+            {
+                newStudent[0] = student;
+            }
+            else if (newStudent[1] == null)
+            {
+                newStudent[1] = student;
+            }
+            else if (newStudent[2] == null)
+            {
+                newStudent[2] = student;
+            }
+            else if (newStudent[3] == null)
+            {
+                newStudent[3] = student;
+            }
+            else if (newStudent[4] == null)
+            {
+                newStudent[4] = student;
+            }
+            else if (newStudent[5] == null)
+            {
+                newStudent[5] = student;
+            }
+            else if (newStudent[6] == null)
+            {
+                newStudent[6] = student;
+            }
+            else if (newStudent[7] == null)
+            {
+                newStudent[7] = student;
+            }
+            else if (newStudent[8] == null)
+            {
+                newStudent[8] = student;
+            }
+            else if (newStudent[9] == null)
+            {
+                newStudent[9] = student;
+            }
+            else if (newStudent[10] == null)
+            {
+                newStudent[10] = student;
+            }
+            else if (newStudent[11] == null)
+            {
+                newStudent[11] = student;
+            }
+            else if (newStudent[12] == null)
+            {
+                newStudent[12] = student;
+            }
+            else if (newStudent[13] == null)
+            {
+                newStudent[13] = student;
+            }
+            else if (newStudent[14] == null)
+            {
+                newStudent[14] = student;
+            }
+            else
+            {
+                for (int index = 0;index > 0; index++)
                 {
-                newStudent[index] = student;
-                    break;               
+                    Console.WriteLine(index);
                 }
-                Console.WriteLine("You enrolled: "+ newStudent);
+
+            } 
+
+
+
+        }
+        static void RemoveStudent()
+        {
+            Console.WriteLine("Enter students name.");
+            string[] newStudent = new string[15];
+            string student = Console.ReadLine();
+
+            for (int index = 0; index < newStudent.Length; index++)
+            {
+                newStudent[index] = null;
+                break;
+            }
+            Console.WriteLine("You removed: " + newStudent);
             for (int index = 0; index < 15; index++)
-                {
-                Console.WriteLine(newStudent[index]);              
-                }
-
-        }
-        
-    }static void RemoveStudent()
-    {
-        Console.WriteLine("Enter students name.");
-        string[] newStudent = new string[15];
-        string student = Console.ReadLine();
-
-        for (int index = 0; index < newStudent.Length; index++)
-        {
-            newStudent[index] = null;
-            break;
-        }
-        Console.WriteLine("You removed: " + newStudent);
-        for (int index = 0; index < 15; index++)
-        {
-            Console.WriteLine(newStudent[index]);
+            {
+                Console.WriteLine(newStudent[index]);
+            }
         }
     }
-}
+}   
 
