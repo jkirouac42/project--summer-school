f266992902dad28a14c70311fd1c251e27b1f702
jkirouac42 <jkirouac42@gmail.com>
2016-06-18 21:54:05 -0400

    second method not complete

--- project-summer-school/Program.cs
+++ project-summer-school/Program.cs
@@ -8,32 +8,63 @@ namespace project_summer_school
 {
     class Program
     {
+        
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to the Hogwart's Student Enrollment System");
-            students = newstring[15]
+            Console.WriteLine("Please select from our menu.\nEnrollment select 1");
+            Console.WriteLine("To remove student select 2");
             while (true)
             {
                 int choice = Convert.ToInt32(Console.ReadLine());
                 if (choice == 1)
                 {
-                    EnrollStudent();//create method
+                    EnrollStudent();//static void method
+                    Console.WriteLine("Should you want to enroll another\nPlease press 1");
+                    Console.WriteLine("To return to the main menu please press 5");
+                }
+                else if (choice == 2)
+                {
+                    RemoveStudent();
                 }
             }
+
         }
         static void EnrollStudent()
         {
             Console.WriteLine("Enter students name.");
             string[] newStudent = new string[15];
             string student = Console.ReadLine();
-            for (int index = 0; index < newStudent.Length; index++)
-            {
-                newStudent[index] = Console.ReadLine();
-                break;
-            }
-
+            
+            for (int index = 0; index < newStudent.Length; index ++)
+                {
+                newStudent[index] = student;
+                    break;               
+                }
+                Console.WriteLine("You enrolled: "+ newStudent);
+            for (int index = 0; index < 15; index++)
+                {
+                Console.WriteLine(newStudent[index]);              
+                }
 
         }
+        
+    }static void RemoveStudent()
+    {
+        Console.WriteLine("Enter students name.");
+        string[] newStudent = new string[15];
+        string student = Console.ReadLine();
 
+        for (int index = 0; index < newStudent.Length; index++)
+        {
+            newStudent[index] = null;
+            break;
+        }
+        Console.WriteLine("You removed: " + newStudent);
+        for (int index = 0; index < 15; index++)
+        {
+            Console.WriteLine(newStudent[index]);
+        }
     }
 }
+
